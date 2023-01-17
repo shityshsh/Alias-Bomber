6302eed84acfe1a56e6f9f6eca2d00cd13f4dd93
DeveloperExodus <101670499+DeveloperExodus@users.noreply.github.com>
2023-01-17 13:52:37 +0900

    Added modules

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ from multiprocessing.pool import ThreadPool
 import threading
 from threading import Thread
 import sib_api_v3_sdk as sib
-from hwid_checker import HWID
+from hwid_logger import HWID
+from email.mime.text import MIMEText
+from tools.functions import clear, system
+from modules.outlook import send_outlook_emails
+from modules.gmail import send_gmail_emails
+import os
 
 
 
@@ -21,61 +26,74 @@ from hwid_checker import HWID
 
 
 
+def ascii():
+    print( Fore.LIGHTRED_EX + f"""\
+    
 
-
-print(Fore.RED + f"""\
+        
     
  
-  ______   __  __                            _______                           __                                               __   
- /      \ /  |/  |                          /       \                         /  |                                            _/  |  
-/$$$$$$  |$$ |$$/   ______    _______       $$$$$$$  |  ______   _____  ____  $$ |____    ______    ______         __     __ / $$ |  
-$$ |__$$ |$$ |/  | /      \  /       |      $$ |__$$ | /      \ /     \/    \ $$      \  /      \  /      \       /  \   /  |$$$$ |  
-$$    $$ |$$ |$$ | $$$$$$  |/$$$$$$$/       $$    $$< /$$$$$$  |$$$$$$ $$$$  |$$$$$$$  |/$$$$$$  |/$$$$$$  |      $$  \ /$$/   $$ |  
-$$$$$$$$ |$$ |$$ | /    $$ |$$      \       $$$$$$$  |$$ |  $$ |$$ | $$ | $$ |$$ |  $$ |$$    $$ |$$ |  $$/        $$  /$$/    $$ |  
-$$ |  $$ |$$ |$$ |/$$$$$$$ | $$$$$$  |      $$ |__$$ |$$ \__$$ |$$ | $$ | $$ |$$ |__$$ |$$$$$$$$/ $$ |              $$ $$/    _$$ |_ 
-$$ |  $$ |$$ |$$ |$$    $$ |/     $$/       $$    $$/ $$    $$/ $$ | $$ | $$ |$$    $$/ $$       |$$ |               $$$/    / $$   |
-$$/   $$/ $$/ $$/  $$$$$$$/ $$$$$$$/        $$$$$$$/   $$$$$$/  $$/  $$/  $$/ $$$$$$$/   $$$$$$$/ $$/                 $/     $$$$$$/ 
-                                                                                                                                     
-                                                                                                                                     
-                                                                                                                                     
+ 
 
+                                    _ _             ____                  _                      __ 
+                              /\   | (_)           |  _ \                | |                    /_ |
+                             /  \  | |_  __ _ ___  | |_) | ___  _ __ ___ | |__   ___ _ __  __   _| |
+                            / /\ \ | | |/ _` / __| |  _ < / _ \| '_ ` _ \| '_ \ / _ \ '__| \ \ / / |
+                           / ____ \| | | (_| \__ \ | |_) | (_) | | | | | | |_) |  __/ |     \ V /| |
+                          /_/    \_\_|_|\__,_|___/ |____/ \___/|_| |_| |_|_.__/ \___|_|      \_/ |_|
 
-                                          {Fore.BLUE + "  Coded By Alias -- github.com/aliasnoclip" + Style.RESET_ALL}                                              
+                                                                           
+                                          {Fore.RED + "  Coded By Alias -- github.com/aliasnoclip" + Style.RESET_ALL}                                              
                                                                                          
-""" + Style.RESET_ALL)
+    
+    """)
+
+
+
+def display_menu():
+    print( Fore.LIGHTRED_EX + """
+    
+    Alias Emailer v1 | Home | alias#9999
+    
+    [1] Gmail
+    
+    [2] Outlook
+    
+    """ + Style.RESET_ALL)
+
+def handle_choice(choice):
+    if choice == "1":
+        send_gmail_emails()
+        pass
+    elif choice == "2":
+        send_outlook_emails()
+        pass
+    elif choice == "3":
+        # code for option 3 v2
+        pass
+    elif choice == "4":
+                clear()
+    elif choice == "5":
+        print("Exiting...")
+        exit()
+    else:
+        print("Invalid choice. Please try again.")
+        time.sleep(2)
+        clear()
+
+
+
+while True:
+    ascii()
+    display_menu()
+    choice = input("Enter your choice: ")
+    handle_choice(choice)
 
 
 
 
-sender_email = ""
-receiver_email = ""
-password = ""
-message = "IDJFHDID PEBVGYJC GGWGFBFI HUOKB"
-num_emails = 1
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(sender_email, password)
 
-def send_email(sender_email, receiver_email, password, message):
-    try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
-        server.quit()
-        print("email sent")
-    except:
-        print("error sending email")    
-threads = []
-for i in range(num_emails):
-    thread = Thread(target=send_email, args=(sender_email, receiver_email, password, message))
-    thread.start()
-    time.sleep(1)
-    threads.append(thread)
 
-for thread in threads:
-    thread.join()
 
-print("All emails sent to "+receiver_email)
 
    
